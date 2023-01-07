3e9f8015f090bba49f9c20e94a3fa2b74f621008
Lizaveta <drago719@mail.ru>
2023-01-08 00:39:21 +0300

    1Sem01_Kv

---
+++ 1Seminar01_Kvadrat/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число A");
+
+int numberA = int.Parse(Console.ReadLine());
+
+double result = Math.Pow(numberA, 2);
+string s = $"квадрат от числа {numberA} равен {result}";
+System.Console.WriteLine(s);
