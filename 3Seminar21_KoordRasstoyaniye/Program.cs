b8712a272d642a81c5e39615af5a89d7a5017369
Lizaveta <drago719@mail.ru>
2023-01-14 02:06:18 +0300

    03S_19

---
+++ 3Seminar21_KoordRasstoyaniye/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите координату x для точки A: ");
+int xA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y для точки A: ");
+int yA = int.Parse(Console.ReadLine());
+Console.Write("Введите координату x для точки B: ");
+int xB = int.Parse(Console.ReadLine());
+Console.Write("Введите координату y для точки B: ");
+int yB = int.Parse(Console.ReadLine());
+double distance = 0;
+
+
+
+distance = Math.Sqrt(Math.Pow(xA - xB, 2) + Math.Pow(yA - yB, 2));
+Console.WriteLine(distance);
